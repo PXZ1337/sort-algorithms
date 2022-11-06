959d50cc31fcdbad3334942e32ae04d84eeb4cde
Marcel Friedrich <marcel.friedrich@spreadgroup.com>
2022-11-06 18:13:26 +0100

    Implementation of bubble_sort in python

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# sort-algorithms
+# Sorting Algorithms
+
 Collection of sorting algorithm implementations, including testing and performance comparison.  
+
+## Bubblesort
+
+Simple sorting algorithm which iterates over each element in the list and compares the current with the next element. 
+
+Performs poorly on big unsorted lists and is primary used as and educational tool.
+
+| Case     | Time complexity      |
+|----------|----------------------|
+| Best     | O on pre sorted list |
+| Worst    | O(n^2)               |
+| Average  | O(n^2)               |
+
+
+### Description
+
+Run [bubble_sort.py](bubble_sort.py).
+
+The script will excute the sorting_algorithm multiple times with an random generated dataset which is increased in size every new iteration. After the execution an assert function is checking the result for consistency.
+
+### Metrics
+
+- Execution time in seconds
+- Number of comparisons
+- Number of switches
---
+++ bubble_sort.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+def bubble_sort(elements: list) -> list:
+    start_time = time.time()
+    s, c = 0, 0
+
+    for (i,_) in enumerate(elements):
+        swapped = False
+        for (j,_) in enumerate(elements[:-(i+1)]): 
+            c += 1
+            if (elements[j] > elements[j+1]):
+                elements[j], elements[j+1] = elements[j+1], elements[j]
+                swapped = True
+                s += 1
+        if not swapped:
+            break
+
+    print(f"--- Compares / Switches: {c} / {s}")
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    return elements
+
+def test_bubble_sort(size_of_sample_set: int):
+    x, y = -size_of_sample_set, size_of_sample_set
+    expectation = random.sample(range(x, y), size_of_sample_set)
+    actual = bubble_sort(expectation)
+    expectation.sort()
+
+    assert actual == expectation
+
+
+if __name__ == '__main__':
+    test_bubble_sort(10)
+    test_bubble_sort(5000)
+    test_bubble_sort(10000)
+    test_bubble_sort(20000)
+    # really slow from here
+    test_bubble_sort(50000)
+    
